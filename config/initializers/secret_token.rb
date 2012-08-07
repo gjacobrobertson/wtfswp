379cc8ec6e0e9d789e1edb8bcd389dd8c08b45ec
Jacob Robertson <jrobertson@quantisense.com>
2012-08-07 17:43:35 -0400

    New Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtfswp::Application.config.secret_token = '931423eb25c5d2fe616595244226e045d7a26307a7eb5740b2652ab98321208e7cd503547b464255d47fd70380860f3809954d149000d1497049f322f4e157fc'
